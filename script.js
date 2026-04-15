0ee4b7bf913fe047d906671179b27750bf344484
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-04-15 17:52:24 +0530

    add script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    const audios = document.querySelectorAll("audio");
+
+    audios.forEach(audio => {
+        audio.addEventListener("play", () => {
+            audios.forEach(otherAudio => {
+                if (otherAudio !== audio) {
+                    otherAudio.pause();
+                    otherAudio.currentTime = 0; // optional (restart)
+                }
+            });
+        });
+    });
